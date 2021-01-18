e5fbd6cd1050bb6bac7e0ef7136cb5fd91a19341
fake name <fakeemail@fakeemailwithfakedomain.com>
2021-01-18 14:05:07 -0800

    pathname placeholder

--- lecture3/hello/urls.py
+++ lecture3/hello/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-	path("", views.index, name="index")
+	path("", views.index, name="index"),
+	path("<str:name>", views.greet, name="greet"),
+	path("joseph", views.joseph, name="joseph")
 ]
--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return HttpResponse("Hello, World")
+
+def joseph(request):
+	return HttpResponse("Hello, Joseph")
+
+def greet(request, name):
+	return HttpResponse("Hello, " + name)
