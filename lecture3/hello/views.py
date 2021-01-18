162a6120dc40ad5478f20d57fc089c5f97e7b2dd
fake name <fakeemail@fakeemailwithfakedomain.com>
2021-01-18 14:22:21 -0800

    passing variables to django templates

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello, World")
+	return render(request, "hello/index.html")
 
 def joseph(request):
 	return HttpResponse("Hello, Joseph")
 
 def greet(request, name):
-	return HttpResponse("Hello, " + name)
+	return render(request, "hello/greet.html",{
+		"name": name.capitalize()
+	})
